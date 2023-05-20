c75ce6ed2fa648102f04e4e6562c300b80a56089
Sarwar Sikder <sarwardevcs@gmail.com>
2023-05-21 02:19:28 +0600

    vue build

--- resources/js/components/example.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Laravel 9 And Vue 3</h1>
-    </div>
-</template>
--- resources/js/layouts/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <NavComponent/>
-    <router-view></router-view>
-    <FooterComponent/>
-</template>
-<script setup>
-import NavComponent from "../components/NavComponent.vue";
-import FooterComponent from "../components/FooterComponent.vue";
-</script>
